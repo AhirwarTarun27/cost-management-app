2196a56faa987c6728b54a195981b8c9a872c8d1
tarun <ahirwar.tarun@thinksys.com>
2023-04-21 17:42:41 +0530

    added wraper and styled the components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
+import Expense from './components/Expense';
+
 function App() {
+  const expense = [
+    {
+      title: 'Fan',
+      amount: 235.5,
+      date: new Date('2023-04-10'),
+    },
+    {
+      title: 'Car',
+      amount: 23545455.5,
+      date: new Date('2023-04-15'),
+    },
+    {
+      title: 'Gun',
+      amount: 233.899,
+      date: new Date('2023-04-20'),
+    },
+  ];
+
   return (
     <div>
       <h2>Let's get started!</h2>
+      <Expense items={expense} />
     </div>
   );
 }
